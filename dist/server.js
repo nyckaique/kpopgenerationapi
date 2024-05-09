34b7dd069b80637cc8d2d4bdf273cf01a0ab7594
Nycollas Kaique <nycollaskaique@hotmail.com>
2024-05-09 17:09:24 -0300

    :package: build: correcao de variavel PORT para deploy no Render.com

--- dist/server.js
+++ dist/server.js
@@ -379,7 +379,7 @@ var start = () => __async(exports, null, function* () {
   try {
     yield app.listen({
       host: "0.0.0.0",
-      port: process.env.PORT ? Number(process.env.port) : 3333
+      port: process.env.PORT ? Number(process.env.PORT) : 3333
     });
   } catch (err) {
     process.exit(1);
--- src/server.ts
+++ src/server.ts
@@ -12,7 +12,7 @@ const start = async () => {
   try {
     await app.listen({
       host: "0.0.0.0",
-      port: process.env.PORT ? Number(process.env.port) : 3333,
+      port: process.env.PORT ? Number(process.env.PORT) : 3333,
     });
   } catch (err) {
     process.exit(1);
